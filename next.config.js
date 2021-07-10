bcf89196a932d1972b5b6d4bde7478e3d33b2776
tawanacode <info@tawanacode.com>
2021-07-10 16:03:48 +0200

    Update basePath

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-const basePath = process.env.NODE_ENV === 'production' ? '/' : '';
+const basePath = ''; //process.env.NODE_ENV === 'production' ? '/nextjs-website' : '';
 
 module.exports = {
   basePath,
