f2b430f7e4a186c42ef56a855118efc062f441f1
tawanacode <info@tawanacode.com>
2021-07-17 23:20:08 +0200

    Deploy Next.js to gh-pages

---
+++ _next/static/oMqWRTYcTvpUusR3Hp5VG/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/975ee0c43dbe9b9c9eaf.css","static/chunks/pages/index-0319531d24a71d2ebf58.js"],"/_error":["static/chunks/pages/_error-9d4115a2b65e99184509.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
