ffeb676392343a863a03a4d89a826a2fe596ac5d
Lesego Magosi <lesego@rampgrowth.com>
2021-12-09 18:59:59 +0200

    Add quotation calculation form

--- components/contacts/quotation-form.js
+++ components/contacts/quotation-form.js
@@ -1,43 +1,112 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function QuotationForm() {
-    
-    const [ checked, setChecked ] = useState(false);
-    const onValueChange = (e) => setChecked(e.currentTarget.checked);
+
+    const [checked, setChecked] = useState(950);
+    const [currentSum, setCurrentSum] = useState(950);
+    const [teamSum, setTeamSum] = useState(0);
+    const [usersSum, setUsersSum] = useState(0);
+    const [adminsSum, setAdminsSum] = useState(0);
+
+    const add = (value) => {
+        const sum = value + teamSum + usersSum + adminsSum;
+        setCurrentSum(sum);
+    }
+    const onValueChange = (e) => {
+        const value = +e.currentTarget.value;
+        setChecked(parseInt(value));
+    };
+
+    const totalUnitPrice = (e) => {
+        const unitId = e.currentTarget.attributes.id.value;
+        switch (unitId) {
+            case 'team':
+                let team = 50 * parseInt(e.currentTarget.value);
+                setTeamSum(team);
+                break;
+            case 'users':
+                const users = 100 * parseInt(e.currentTarget.value);
+                setUsersSum(users);
+                break;
+            case 'admins':
+                const admins = 200 * parseInt(e.currentTarget.value);
+                setAdminsSum(admins);
+                break;
+            default:
+                console.log(`Sorry, we are out of ${unitId}.`);
+        }
+    }
+
+
+    useEffect(() => {
+        add(checked);
+      },[checked, teamSum, usersSum, adminsSum])
 
     return <form id="contactForm" data-sb-form-api-token="API_TOKEN">
 
-        <div className="form-floatig mb-3">
-            <input type="radio" className="btn-check" name="pricing-plan" id="individual" autoComplete="off"  onChange={onValueChange} value="0" defaultChecked />
-            <label className="btn btn-outline-primary" htmlFor="individual">Individual</label>
+        <div className="form-floatig mb-3 d-flex justify-content-around">
+            <input type="radio" className="btn-check" name="pricing-plan" id="individual" autoComplete="off" onChange={onValueChange} value="950" defaultChecked />
+            <label className="btn btn-outline-primary" htmlFor="individual">Individual <br /> P950</label>
 
-            <input type="radio" className="btn-check" name="pricing-plan" id="small" autoComplete="off"  onChange={onValueChange} value="0" />
-            <label className="btn btn-outline-primary" htmlFor="small">Small Law Firm</label>
+            <input type="radio" className="btn-check" name="pricing-plan" id="small" autoComplete="off" onChange={onValueChange} value="2000" />
+            <label className="btn btn-outline-primary" htmlFor="small">Small Law Firm <br /> P2000</label>
 
-            <input type="radio" className="btn-check" name="pricing-plan" id="large" autoComplete="off"  onChange={onValueChange} value="0" />
-            <label className="btn btn-outline-primary" htmlFor="large">Large Law Firm</label>
+            <input type="radio" className="btn-check" name="pricing-plan" id="large" autoComplete="off" onChange={onValueChange} value="5000" />
+            <label className="btn btn-outline-primary" htmlFor="large">Large Law Firm <br /> P5000</label>
         </div>
-        {/* additional team members input */}
-        <div className="form-floating mb-3">
-            <input className="form-control" id="team" type="number" placeholder="Extra team members..." />
-            <label htmlFor="team">Number of extra team members</label>
-            <div className="invalid-feedback" data-sb-feedback="team:required">A name is required.</div>
+        <div className="row mb-3 justify-content-between align-items-center">
+
+            <h6>Add extra team members</h6>
+            {/* additional team members input */}
+            <div className="col-2">
+                <label htmlFor="team" className="d-none">Add extra team members</label>
+                <input className="form-control" id="team" type="number" placeholder="0" onChange={totalUnitPrice} min="0" max="100"/>
+                <div className="invalid-feedback" data-sb-feedback="team:required">A name is required.</div>
+            </div>
+            <div className="col-4">x R50</div>
+            <div className="col-2">
+                <input type="text" readOnly className="form-control-plaintext text-center p-0" id="addTeam" value={teamSum} />
+            </div>
         </div>
-        {/* additional normal users input */}
-        <div className="form-floating mb-3">
-            <input className="form-control" id="users" type="number" placeholder="Enter your name..." data-sb-validations="required" />
-            <label htmlFor="users">Number of extra Normal Users</label>
-            <div className="invalid-feedback" data-sb-feedback="name:required">A name is required.</div>
+        <div className="row mb-3 justify-content-between align-items-center">
+            <h6>Add extra Normal User logins</h6>
+            {/* additional normal users input */}
+            <div className="col-2">
+                <label htmlFor="users" className="d-none">Add extra Normal User logins</label>
+                <input className="form-control" id="users" type="number" placeholder="0"  onChange={totalUnitPrice} min="0" max="100" />
+                <div className="invalid-feedback" data-sb-feedback="name:required">A name is required.</div>
+            </div>
+            <div className="col-4">x R100</div>
+            <div className="col-2">
+                <input type="text" readOnly className="form-control-plaintext text-center p-0" id="addUsers" value={usersSum} />
+            </div>
         </div>
-        {/* additional admins input */}
-        <div className="form-floating mb-3">
-            <input className="form-control" id="admins" type="number" placeholder="Enter your name..." data-sb-validations="required" />
-            <label htmlFor="admins">Number of extra Admin Users</label>
-            <div className="invalid-feedback" data-sb-feedback="name:required">A name is required.</div>
+        <div className="row mb-5 justify-content-between align-items-center">
+            <h6>Add extra Admin User logins</h6>
+            {/* additional admins input */}
+            <div className="col-2">
+                <label htmlFor="admins" className="d-none">Add extra Admin User logins</label>
+                <input className="form-control" id="admins" type="number" placeholder="0"  onChange={totalUnitPrice} min="0" max="100"/>
+                <div className="invalid-feedback" data-sb-feedback="name:required">A name is required.</div>
+            </div>
+            <div className="col-4 text-mute">x R200</div>
+            <div className="col-2">
+                <input type="text" readOnly className="form-control-plaintext text-center p-0" id="addAdmins" value={adminsSum} />
+            </div>
         </div>
+        <hr />
+        <div className="row mb-3 justify-content-between align-items-center">
+            <div className="col-6">
+                <label htmlFor="total" className="col-sm-2 col-form-label">Total</label>
+            </div>
+            <div className="col-2">
+                <input type="text" readOnly className="form-control-plaintext text-center" id="total" value={currentSum} />
+            </div>
+        </div>
+        <hr />
         {/* Name input */}
-        <div className="form-floating mb-3">
-            <input className="form-control" id="name" type="text" placeholder="Enter your name..." data-sb-validations="required" />
+        <div className="form-floating my-3">
+            <input className="form-control" id="name" type="text" placeholder="0" data-sb-validations="required" />
             <label htmlFor="name">Full name</label>
             <div className="invalid-feedback" data-sb-feedback="name:required">A name is required.</div>
         </div>
@@ -71,6 +140,6 @@ export default function QuotationForm() {
         {/* an error submitting the form */}
         <div className="d-none" id="submitErrorMessage"><div className="text-center text-danger mb-3">Error sending message!</div></div>
         {/* Submit Button */}
-        <div className="d-grid"><button className="btn btn-primary btn-lg disabled" id="submitButton" type="submit">Submit</button></div>
+        <div className="d-grid"><button className="btn btn-primary btn disabled" id="submitButton" type="submit">Submit</button></div>
     </form>
 }
--- pages/contact.js
+++ pages/contact.js
@@ -21,7 +21,7 @@ export default function Contact() {
 
             <section className="bg-light py-5">
                 <div className="container px-5 my-5">
-                    <ul className="nav nav-tabs justify-content-center mb-5" id="pills-tab" role="tablist">
+                    <ul className="nav nav-tabs justify-content-center" id="pills-tab" role="tablist">
                         <li className="nav-item" role="presentation">
                             <button className="nav-link active" id="pills-contacts-tab" data-bs-toggle="pill" data-bs-target="#pills-contacts" type="button" role="tab" aria-controls="pills-contacts" aria-selected="true">contacts</button>
                         </li>
@@ -29,7 +29,7 @@ export default function Contact() {
                             <button className="nav-link" id="pills-quotation-tab" data-bs-toggle="pill" data-bs-target="#pills-quotation" type="button" role="tab" aria-controls="pills-quotation" aria-selected="false">Request A Quote</button>
                         </li>
                     </ul>
-                    <div className="tab-content" id="pills-tabContent">
+                    <div className="tab-content p-5 bg-white" id="pills-tabContent">
 
                         <div className="tab-pane fade show active" id="pills-contacts" role="tabpanel" aria-labelledby="pills-contacts-tab">
                             <div className="row">
